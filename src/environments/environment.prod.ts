44c3e0c1c2ebefd94cfec1a8ada2037c565a4173
Alex Green <imagi.soft@gmail.com>
2019-11-29 04:35:16 +0000

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-S1lF7ckEc4ECovOBFWtbiFSvmEMjEh0",
+    authDomain: "clouds-baf31.firebaseapp.com",
+    databaseURL: "https://clouds-baf31.firebaseio.com",
+    projectId: "clouds-baf31",
+    storageBucket: "clouds-baf31.appspot.com",
+    messagingSenderId: "829508636398",
+    appId: "1:829508636398:web:2fbf14877f1bece77bedff",
+    measurementId: "G-QCWXK1RFFC"
+
+  }
 };
